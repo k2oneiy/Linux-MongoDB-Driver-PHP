c6bd115201ed55deb1535572108e74ba398050f1
k2oneiy <kiyappan104@gmail.com>
2021-12-15 17:41:48 +0530

    php mongodb crud in linux

---
+++ delete.php
@@ -0,0 +1,42 @@
+
+<?php
+include 'vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Mongo DB</title>
+</head>
+<body>
+    <h2>Mongo DB</h2>
+    <h4>delete</h4>
+    <form action="delete.php" method="post">
+        <h4>LOGIN</h4><br>
+        <h4>Name</h4>
+        <input type="text" name="name"><br>
+        <h4>Password</h4>
+        <!-- <input type="password" name="password"><br><br><br> -->
+        <input type="submit" value="login" name="login">
+    </form>
+    
+</body>
+</html>
+
+<?php
+if(isset($_POST["login"])){
+    $name = $_POST["name"];
+    // $password = $_POST["password"];
+    $client = new MongoDB\Client("mongodb://127.0.0.1:27017");
+    $db = $client->k2oneiy;
+    $collection = $db->login;
+    $res = $collection->drop(['name'=>$name]);
+}
+
+
+
+?>
